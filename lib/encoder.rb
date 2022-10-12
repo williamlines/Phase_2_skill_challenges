de42cf0a2fad34c0c88378db666fedfc70c51ed5
William Lines <williamlines42@gmail.com>
2022-10-12 12:31:04 +0100

    partially debugged encoder

--- lib/encoder.rb
+++ lib/encoder.rb
@@ -1,16 +1,20 @@
 def encode(plaintext, key)
-  cipher = key.chars.uniq + (('a'...'z').to_a - key.chars)
-  ciphertext_chars = plaintext.chars.map.each do |char|
+  cipher = key.chars.uniq + (('a'..'z').to_a - key.chars)
+ # p "cipher equals #{cipher}"
+  ciphertext_chars = plaintext.chars.map do |char|
     (65 + cipher.find_index(char)).chr
   end
+  #p "ciphertext_chars equals #{ciphertext_chars}"
   return ciphertext_chars.join
 end
 
 def decode(ciphertext, key)
-  cipher = key.chars.uniq + (('a'...'z').to_a - key.chars)
-  plaintext_chars = ciphertext.chars.map.each do |char|
+  cipher = key.chars.uniq + (('a'..'z').to_a - key.chars)
+  #  p "cipher equals #{cipher}"
+  plaintext_chars = ciphertext.chars.map do |char|
     cipher[65 - char.ord]
   end
+  p "plaintext equals #{plaintext_chars.join}"
   return plaintext_chars.join
 end
 
@@ -21,3 +25,5 @@ end
 #
 # > decode("EMBAXNKEKSYOVQTBJSWBDEMBPHZGJSL", "secretkey")
 # => "theswiftfoxjumpedoverthelazydog"
+#encode("theswiftfoxjumpedoverthelazydog", "secretkey")
+decode("EMBAXNKEKSYOVQTBJSWBDEMBPHZGJSL", "secretkey")
